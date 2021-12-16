e8a34decd31790b8fe619bc5064e82bdff873599
John Doe <luschykoleg@gmail.com>
2021-12-16 16:25:07 +0200

    added new block and scss

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-window.$ = window.jQuery = require('jquery');
+ window.$ = window.jQuery = require('jquery');
 import Swiper, { Autoplay, Navigation } from 'swiper';
 
 Swiper.use([ Autoplay, Navigation ]);
